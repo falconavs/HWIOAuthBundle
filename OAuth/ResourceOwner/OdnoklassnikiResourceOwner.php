4ffd95ddb5a62958fbe66d94f013885600941a20
avs <avs@htc-cs.com>
2015-10-16 11:04:42 +0400

    fix for odnoklassniki resource owner

--- OAuth/ResourceOwner/OdnoklassnikiResourceOwner.php
+++ OAuth/ResourceOwner/OdnoklassnikiResourceOwner.php
@@ -38,7 +38,15 @@ class OdnoklassnikiResourceOwner extends GenericOAuth2ResourceOwner
         $parameters = array(
             'access_token'    => $accessToken['access_token'],
             'application_key' => $this->options['application_key'],
-            'sig'             => md5(sprintf('application_key=%smethod=users.getCurrentUser%s', $this->options['application_key'], md5($accessToken['access_token'].$this->options['client_secret']))),
+            'fields'          => $this->options['fields'],
+            'sig'             => strtolower(md5(
+                sprintf(
+                    'application_key=%sfields=%smethod=users.getCurrentUser%s',
+                    $this->options['application_key'],
+                    $this->options['fields'],
+                    md5($accessToken['access_token'] . $this->options['client_secret'])
+                )
+            )),
         );
         $url = $this->normalizeUrl($this->options['infos_url'], $parameters);
 
@@ -60,11 +68,12 @@ class OdnoklassnikiResourceOwner extends GenericOAuth2ResourceOwner
         parent::configureOptions($resolver);
 
         $resolver->setDefaults(array(
-            'authorization_url' => 'http://www.odnoklassniki.ru/oauth/authorize',
-            'access_token_url'  => 'http://api.odnoklassniki.ru/oauth/token.do',
-            'infos_url'         => 'http://api.odnoklassniki.ru/fb.do?method=users.getCurrentUser',
+            'authorization_url' => 'https://connect.ok.ru/oauth/authorize',
+            'access_token_url'  => 'https://api.odnoklassniki.ru/oauth/token.do',
+            'infos_url'         => 'https://api.ok.ru/fb.do?method=users.getCurrentUser',
 
             'application_key'   => null,
+            'fields'            => "user.*",
         ));
     }
 }
